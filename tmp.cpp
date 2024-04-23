275f3a1e5575c494ad6250715b6cf734486c62d7
Benjamin Taylor <taylor.be@northeastern.edu>
2024-04-23 16:36:20 -0400

    Working inputs??

--- tmp.cpp
+++ tmp.cpp
@@ -1,19 +1,24 @@
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 #include <ap_utils.h>
+#include "hls_math.h"
 
 typedef ap_axis<64,1,1,1> AXI_VAL;
 typedef ap_axis<32,1,1,1> RET_VAL;
-typedef ap_int<32> uint32_t;
-typedef ap_int<16> uint16_t;
-typedef ap_fixed<16, 16, AP_TRN, AP_SAT> frac_t;
+typedef ap_fixed<8, 0, AP_TRN, AP_SAT> frac_t;
 // TODO: revisit these types
-typedef ap_fixed<32, 16, AP_TRN, AP_SAT> data_t;
+typedef ap_fixed<16, 8, AP_TRN, AP_SAT> data_t;
 typedef ap_fixed<32, 16, AP_TRN, AP_SAT> tensor_t;
 
 // Placeholder type
-typedef ap_int<64> payload_t;
-typedef ap_fixed<32, 16, AP_TRN, AP_SAT> coef_t;
+typedef long long payload_t;
+typedef ap_fixed<16, 8, AP_TRN, AP_SAT> coef_t;
+
+typedef union
+{
+	float f;
+	uint32_t ui;
+} input_conversion;
 
 #define WIN_WIDTH 4
 #define N_BINS 8
@@ -30,9 +35,17 @@ void histogram_tensor (hls::stream<AXI_VAL>& x, hls::stream<RET_VAL>& y) {
 //#pragma HLS ARRAY_PARTITION dim=2 factor=1 type=block variable=histogram_tensor
 //#pragma HLS ARRAY_PARTITION dim=3 factor=1 type=block  variable=histogram_tensor
 
-
+  // Additional flags
+  ap_uint<4> TKEEP_on = 0xf;
+  ap_uint<4> TSTRB_on = 0xf;
+  ap_uint<4> TKEEP_off = 0x0;
+  ap_uint<4> TSTRB_off = 0x0;
 
   RET_VAL output;
+
+  // All current data should not count as data
+  output.keep = TKEEP_off;
+  output.strb = TSTRB_off;
 
   while(1) {
 #pragma HLS PIPELINE II=8
@@ -45,25 +58,42 @@ void histogram_tensor (hls::stream<AXI_VAL>& x, hls::stream<RET_VAL>& y) {
 	// orientation_bin
 	x.read(tmp1);
 
-	// Let each of the values exist along 32 bits
-	uint32_t bitmask = 0xFFFFFFFF;
+	// Let each of the values exist along 16 bits
+	int16_t bitmask = 0xFFFF;
+	int16_t sign_bitmask = 0x8000;
+	int16_t exp_bitmask = 0x7A00;
+	int16_t mantissa_bitmask = 0x03FF;
+	input_conversion ic;
 	payload_t data = tmp1.data;
-	half row_bin_f = (data >> 0) & bitmask;
-	half col_bin_f = (data >> 16) & bitmask;
-	half magnitude_f = (data >> 32) & bitmask;
-	half orientation_bin_f = (data >> 48) & bitmask;
-	data_t row_bin = (data_t) row_bin_f;
-	data_t col_bin = (data_t) col_bin_f;
-	data_t magnitude = (data_t) magnitude_f;
-	data_t orientation_bin = (data_t) orientation_bin_f;
+
+	int16_t in_f = (data >> 0) & bitmask;
+	ic.ui = ((in_f&sign_bitmask)<<16) | (((in_f&exp_bitmask)+0x1C000)<<13) | ((in_f&mantissa_bitmask)<<13);
+	half row_bin_h = ic.f;
+
+	in_f = (data >> 16) & bitmask;
+	ic.ui = ((in_f&sign_bitmask)<<16) | (((in_f&exp_bitmask)+0x1C000)<<13) | ((in_f&mantissa_bitmask)<<13);
+	half col_bin_h = ic.f;
+
+	in_f = (data >> 32) & bitmask;
+	ic.ui = ((in_f&sign_bitmask)<<16) | (((in_f&exp_bitmask)+0x1C000)<<13) | ((in_f&mantissa_bitmask)<<13);
+	half magnitude_h = ic.f;
+
+	in_f = (data >> 48) & bitmask;
+	ic.ui = ((in_f&sign_bitmask)<<16) | (((in_f&exp_bitmask)+0x1C000)<<13) | ((in_f&mantissa_bitmask)<<13);
+	half orientation_bin_h = ic.f;
+
+	data_t row_bin = (data_t) row_bin_h;
+	data_t col_bin = (data_t) col_bin_h;
+	data_t magnitude = (data_t) magnitude_h;
+	data_t orientation_bin = (data_t) orientation_bin_h;
 
 	// split the row_bin and col_bin into their integer and fractional parts
-	uint16_t row_int = row_bin >> 16;
-	frac_t row_fraction = row_bin & 0xFFFF;
-	uint16_t col_int = col_bin >> 16;
-	frac_t col_fraction = col_bin & 0xFFFF;
-	uint16_t orientation_bin_int = orientation_bin >> 16;
-	frac_t orientation_fraction = orientation_bin & 0xFFFF;
+	int8_t row_int = row_bin >> 8;
+	frac_t row_fraction = row_bin & 0xFF;
+	int8_t col_int = col_bin >> 8;
+	frac_t col_fraction = col_bin & 0xFF;
+	int8_t orientation_bin_int = orientation_bin >> 8;
+	frac_t orientation_fraction = orientation_bin & 0xFF;
 
 	if (orientation_bin_int < 0) {
 		orientation_bin_int += N_BINS;
@@ -91,14 +121,14 @@ void histogram_tensor (hls::stream<AXI_VAL>& x, hls::stream<RET_VAL>& y) {
 	coef_t c000 = c00 * (1 - orientation_fraction);
 
 	// Add tensors
-	data_t read1 = histogram_tensor[row_int + 1][col_int + 1][orientation_bin_int];
-	data_t read2 = histogram_tensor[row_int + 1][col_int + 1][(orientation_bin_int + 1) % N_BINS];
-	data_t read3 = histogram_tensor[row_int + 1][col_int + 2][orientation_bin_int];
-	data_t read4 = histogram_tensor[row_int + 1][col_int + 2][(orientation_bin_int + 1) % N_BINS];
-	data_t read5 = histogram_tensor[row_int + 2][col_int + 1][orientation_bin_int];
-	data_t read6 = histogram_tensor[row_int + 2][col_int + 1][(orientation_bin_int + 1) % N_BINS];
-	data_t read7 = histogram_tensor[row_int + 2][col_int + 2][orientation_bin_int];
-	data_t read8 = histogram_tensor[row_int + 2][col_int + 2][(orientation_bin_int + 1) % N_BINS];
+	tensor_t read1 = histogram_tensor[row_int + 1][col_int + 1][orientation_bin_int];
+	tensor_t read2 = histogram_tensor[row_int + 1][col_int + 1][(orientation_bin_int + 1) % N_BINS];
+	tensor_t read3 = histogram_tensor[row_int + 1][col_int + 2][orientation_bin_int];
+	tensor_t read4 = histogram_tensor[row_int + 1][col_int + 2][(orientation_bin_int + 1) % N_BINS];
+	tensor_t read5 = histogram_tensor[row_int + 2][col_int + 1][orientation_bin_int];
+	tensor_t read6 = histogram_tensor[row_int + 2][col_int + 1][(orientation_bin_int + 1) % N_BINS];
+	tensor_t read7 = histogram_tensor[row_int + 2][col_int + 2][orientation_bin_int];
+	tensor_t read8 = histogram_tensor[row_int + 2][col_int + 2][(orientation_bin_int + 1) % N_BINS];
 
 //#pragma HLS PROTOCOL mode=fixed
 
@@ -123,15 +153,13 @@ void histogram_tensor (hls::stream<AXI_VAL>& x, hls::stream<RET_VAL>& y) {
   int dim2 = (WIN_WIDTH + 2);
   int dim3 = N_BINS;
 
-  // Additional flags
-  ap_uint<4> TKEEP = 0xf;
-  ap_uint<4> TSTRB = 0xf;
-
   while (dim1 >= 0) {
-	  output.data = (float)histogram_tensor[dim1][dim2][dim3];
+	  output.data = (float) 1.10123;
+
+	  //	  output.data = histogram_tensor[dim1][dim2][dim3].to_float();
 	  output.last = 0;
-	  output.keep = TKEEP;
-	  output.strb = TSTRB;
+	  output.keep = TKEEP_on;
+	  output.strb = TSTRB_on;
 
 	  if (dim3 > 0) {
 		  dim3 -= 1;
@@ -147,7 +175,7 @@ void histogram_tensor (hls::stream<AXI_VAL>& x, hls::stream<RET_VAL>& y) {
 	  }
 	  y.write(output);
   }
-  output.data = (float)histogram_tensor[0][0][0];
+  output.data = histogram_tensor[0][0][0].to_float();
   output.last = 1;
   y.write(output);
 }
